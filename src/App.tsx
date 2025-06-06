7a7e5c9377568de3963b7f3d0a589b39d1d20824
Kars Talens <karstalens@gmail.com>
2025-06-06 18:43:03 +0200

    install hotjar

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,13 @@ import { Outlet, Route } from 'react-router';
 import type { Navigation } from '@toolpad/core';
 import theme from './theme';
 import { Analytics } from "@vercel/analytics/react";
+import Hotjar from "@hotjar/browser";
+
+const siteId = 6427982;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
+
 const NAVIGATION: Navigation = [
   {
     kind: "header",
